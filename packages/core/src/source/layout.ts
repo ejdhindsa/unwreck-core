96f034f793d975328f0ac6d48b7c147a28013917
ejdhindsa <ejdhindsa@Gmail.com>
2026-08-17 21:20:40 -0500

    feature(tokens): Set up non-color tokens for core

---
+++ packages/core/src/source/layout.ts
@@ -0,0 +1,18 @@
+export const breakpoint = {
+  sm: '40rem',
+  md: '48rem',
+  lg: '64rem',
+  xl: '80rem',
+  '2xl': '96rem',
+}
+
+export const z = {
+  base: '0',
+  dropdown: '1000',
+  sticky: '1100',
+  overlay: '1200',
+  modal: '1300',
+  popover: '1400',
+  toast: '1500',
+  tooltip: '1600',
+}
