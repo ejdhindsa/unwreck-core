de45b409f7101d7fb257c4618346fbe5abf53a51
ejdhindsa <ejdhindsa@Gmail.com>
2026-08-17 21:20:40 -0500

    feature(tokens): Set up non-color tokens for core

---
+++ packages/core/src/source/motion.ts
@@ -0,0 +1,14 @@
+export const duration = {
+  instant: '0ms',
+  fast: '120ms',
+  normal: '200ms',
+  slow: '320ms',
+  slower: '500ms',
+}
+
+export const easing = {
+  standard: 'cubic-bezier(0.2, 0, 0, 1)',
+  emphasized: 'cubic-bezier(0.3, 0, 0, 1)',
+  decelerate: 'cubic-bezier(0, 0, 0, 1)',
+  accelerate: 'cubic-bezier(0.3, 0, 1, 1)',
+}
